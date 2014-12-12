0ab3fb30206b444fa79e5b7e70f1c3dd3c657904
Jeongmin Byun <jmbyun91@gmail.com>
2014-12-12 09:40:50 +0900

    Updated logo.

--- mockup/js/jquery.uilab-active-logo.js
+++ mockup/js/jquery.uilab-active-logo.js
@@ -1,126 +1,224 @@
 
-var uilabLogo = window.uilabLogo || {};
-
-uilabLogo.setStyle = function($container) {
-	$container.css({
-		'display': 'block',
+var UILabLogo = function($container) {
+	this.$container = $container;
+	this.setStyle().setConfig().setLengthes();
+	this.createDots();
+	return this;
+};
+UILabLogo.prototype.setStyle = function() {
+	this.$container.css({
 		'position': 'relative'
 	});
+	return this;
 };
-
-uilabLogo.getLengthes = function(containerSize, matrixSize, smallDotRatio, bigDotRatio) {
-	var smallDotSize = containerSize * smallDotRatio;
-	var bigDotSize = containerSize * bigDotRatio;
+UILabLogo.prototype.setConfig = function() {
+	this.config = {
+		'matrixSize': 4,
+		'smallDotRatio': 0.07,
+		'bigDotRatio': 0.16,
+		'defaultAnimationInterval': 500,
+	};
+	return this;
+};
+UILabLogo.prototype.setLengthes = function() {
+	var containerSize = this.$container.width();
+	var smallDotSize = containerSize * this.config.smallDotRatio;
+	var bigDotSize = containerSize * this.config.bigDotRatio;
 	var outerMargin = bigDotSize * 0.5;
-	var innerMargin = (containerSize - (2 * outerMargin)) / (matrixSize - 1);
+	var innerMargin = (containerSize - (2 * outerMargin)) / (this.config.matrixSize - 1);
 
-	// return {
-	// 	'outerMargin': outerMargin,
-	// 	'innerMargin': innerMargin,
-	// 	'smallDotSize': smallDotSize,
-	// 	'bigDotSize': bigDotSize
-	// };
-	return {
+	this.lengthes = {
 		'outerMargin': Math.round(outerMargin),
 		'innerMargin': Math.round(innerMargin),
 		'smallDotSize': Math.round(smallDotSize),
 		'bigDotSize': Math.round(bigDotSize)
 	};
-	
+	return this;
 };
-
-uilabLogo.createDot = function($container, className, x, y, size) {
-	$container.append('<div class="' + className + '"></div>');
-	var dot = $container.find('.' + className);
+UILabLogo.prototype.createDot = function(i, j, x, y) {
+	var className = 'dot-' + i + '-' + j;
+	this.$container.append('<div class="' + className + '"></div>');
+	var dot = this.$container.find('.' + className);
+	var size = this.lengthes.smallDotSize;
 	dot.css({
 		'position': 'absolute',
 		'top': y - (size * 0.5) + 'px',
 		'left': x - (size * 0.5) + 'px',
 		'width': size + 'px',
 		'height': size + 'px',
-		// 'border-radius': size * 0.5 + 'px',
 		'border-radius': '50%',
 		'background-color': '#333333',
-		'transition': 'all 0.3s ease-in-out',
+		'transition': 'all 0.2s ease-in-out',
 	});
 	return dot;
 };
-
-uilabLogo.increaseDot = function($container, dots, lengthes, i, j) {
-	var x = lengthes.outerMargin + j * lengthes.innerMargin;
-	var y = lengthes.outerMargin + i * lengthes.innerMargin;
-	var size = lengthes.bigDotSize;
-	dots[i][j].css({
-		'width': lengthes.bigDotSize,
-		'height': lengthes.bigDotSize,
+UILabLogo.prototype.changeDotSize = function(i, j, size) {
+	var x = this.lengthes.outerMargin + j * this.lengthes.innerMargin;
+	var y = this.lengthes.outerMargin + i * this.lengthes.innerMargin;
+	this.dots[i][j].css({
+		'width': size,
+		'height': size,
 		'top': y - (size * 0.5) + 'px',
 		'left': x - (size * 0.5) + 'px',
 	});
+	return this;
 };
-
-uilabLogo.decreaseDot = function($container, dots, lengthes, i, j) {
-	var x = lengthes.outerMargin + j * lengthes.innerMargin;
-	var y = lengthes.outerMargin + i * lengthes.innerMargin;
-	var size = lengthes.smallDotSize;
-	dots[i][j].css({
-		'width': lengthes.bigDotSize,
-		'height': lengthes.bigDotSize,
-		'top': y - (size * 0.5) + 'px',
-		'left': x - (size * 0.5) + 'px',
-	});
+UILabLogo.prototype.increaseDot = function(i, j) {
+	return this.changeDotSize(i, j, this.lengthes.bigDotSize);
 };
-
-uilabLogo.createDots = function($container) {
-	var i, j;
-	var matrixSize = 4;
-	var smallDotRatio = 0.07;
-	var bigDotRatio = 0.20;
-	// var maxDotRatio = 0.25;
-
-	var containerSize = $container.width();
-	var lengthes = uilabLogo.getLengthes(containerSize, matrixSize, smallDotRatio, bigDotRatio);
-
-	var dots = [];
-	var x, y, size;
-	for (i = 0; i < matrixSize; i++) {
+UILabLogo.prototype.decreaseDot = function(i, j) {
+	return this.changeDotSize(i, j, this.lengthes.smallDotSize);
+};
+UILabLogo.prototype.changeDotColor = function(i, j, color) {
+	this.dots[i][j].css({'background-color': color});
+	return this;
+};
+UILabLogo.prototype.createDots = function() {
+	var i, j, x, y, size;
+	this.dots = [];
+	for (i = 0; i < this.config.matrixSize; i++) {
 		var dotLine = []
-		for (j = 0; j < matrixSize; j++) {
-			x = lengthes.outerMargin + j * lengthes.innerMargin;
-			y = lengthes.outerMargin + i * lengthes.innerMargin;
-			size = lengthes.smallDotSize;
-			dotLine.push(uilabLogo.createDot($container, 'dot-' + i + '-' + j, x, y, size))
+		for (j = 0; j < this.config.matrixSize; j++) {
+			x = this.lengthes.outerMargin + j * this.lengthes.innerMargin;
+			y = this.lengthes.outerMargin + i * this.lengthes.innerMargin;
+			size = this.lengthes.smallDotSize;
+			dotLine.push(this.createDot(i, j, x, y))
 		}
-		dots.push(dotLine);
+		this.dots.push(dotLine);
 	}
-
-	$.data($container, 'lengthes', lengthes);
-	$.data($container, 'dots', dots);
-	
+	return this;
+};
+UILabLogo.prototype.startDefaultAnimation = function() {
+	var logo = this;
 	setTimeout(function(){
-		uilabLogo.increaseDot($container, dots, lengthes, 1, 0);
-		dots[1][0].css('background-color', '#ed0000');
+		logo.increaseDot(0, 1);
+		logo.changeDotColor(0, 1, '#1C3D98');
 	 }, 500);
 	setTimeout(function(){
-		uilabLogo.increaseDot($container, dots, lengthes, 1, 1);
-		
+		logo.increaseDot(1, 1);
 	 }, 1000);
 	setTimeout(function(){
-		uilabLogo.increaseDot($container, dots, lengthes, 1, 2);
-		uilabLogo.increaseDot($container, dots, lengthes, 2, 2);
+		logo.increaseDot(2, 1);
+		logo.increaseDot(2, 2);
 	 }, 1500);
 	setTimeout(function(){
-		uilabLogo.increaseDot($container, dots, lengthes, 1, 3);
-		uilabLogo.increaseDot($container, dots, lengthes, 3, 3);
+		logo.increaseDot(3, 1);
+		logo.increaseDot(3, 3);
 	 }, 2000);
 };
+UILabLogo.prototype.randInt = function(a, b) {
+	return Math.floor((Math.random() * b) + a);
+};
+UILabLogo.prototype.randColor = function() {
+	var colors = [
+		'#1C3D98', '#4285F4', '#66D04F', '#FEEC4A', '#E32170', '#D4161B', '#2CA48F'
+	];
+	var randIndex = this.randInt(0, colors.length);
+	return colors[randIndex];
+};
+UILabLogo.prototype.drawRandomAnimation = function() {
+	var logo = this;
+	var increase = this.randInt(1,2);
+	var vertical = this.randInt(1,2);
+	var branch = this.randInt(1,2);
+	var interval = this.config.defaultAnimationInterval;
+	var points = [];
+	var colorCode = this.randColor();
 
-uilabLogo.drawLines = function() {
+	for (var i = 0; i < 4; i++) {
+		pointSet = [-1, -1, -1];
+		if (increase == 1) pointSet[0] = i;
+		else pointSet[0] = 3 - i;
 
+		if (branch == 1) {
+			if (i == 0 || i == 1) {
+				pointSet[1] = 1;
+				pointSet[2] = -1;
+			}
+			else if (i == 2) {
+				pointSet[1] = 1;
+				pointSet[2] = 2;
+			}
+			else {
+				pointSet[1] = 1;
+				pointSet[2] = 3;
+			}
+		}
+		else {
+			if (i == 0 || i == 1) {
+				pointSet[1] = 2;
+				pointSet[2] = -1;
+			}
+			else if (i == 2) {
+				pointSet[1] = 2;
+				pointSet[2] = 1;
+			}
+			else {
+				pointSet[1] = 2;
+				pointSet[2] = 0;
+			}	
+		}
+		points.push(pointSet);
+	}
+	setTimeout(function(){
+		if (vertical == 1) {
+			logo.increaseDot(points[0][0], points[0][1]);
+			logo.changeDotColor(points[0][0], points[0][1], colorCode);
+		}
+		else {
+			logo.increaseDot(points[0][1], points[0][0]);	
+			logo.changeDotColor(points[0][1], points[0][0], colorCode);
+		}
+	}, interval);
+	setTimeout(function(){
+		if (vertical == 1) {
+			logo.increaseDot(points[1][0], points[1][1]);
+		}
+		else {
+			logo.increaseDot(points[1][1], points[1][0]);	
+		}
+	}, interval * 2);
+	setTimeout(function(){
+		if (vertical == 1) {
+			logo.increaseDot(points[2][0], points[2][1]);
+			logo.increaseDot(points[2][0], points[2][2]);
+		}
+		else {
+			logo.increaseDot(points[2][1], points[2][0]);	
+			logo.increaseDot(points[2][2], points[2][0]);	
+		}
+	}, interval * 3);
+	setTimeout(function(){
+		if (vertical == 1) {
+			logo.increaseDot(points[3][0], points[3][1]);
+			logo.increaseDot(points[3][0], points[3][2]);
+		}
+		else {
+			logo.increaseDot(points[3][1], points[3][0]);	
+			logo.increaseDot(points[3][2], points[3][0]);	
+		}
+	}, interval * 4);
+	setTimeout(function(){
+		for (var i = 0; i < 4; i++) {
+			for (var j = 0; j < 4; j++) {
+				logo.decreaseDot(i, j);	
+				logo.changeDotColor(i, j, '#000000');
+			}
+		}
+	}, interval * 12);
+};
+UILabLogo.prototype.startDefaultRandomAnimation = function() {
+	var interval = this.config.defaultAnimationInterval;
+	var logo = this;
+	this.drawRandomAnimation();
+
+	setTimeout(function() {
+		logo.startDefaultRandomAnimation();
+	}, interval * 13)
 };
 
 $.fn.extend({
-	fillUilabLogo: function() {
-		uilabLogo.createDots(this);
-		return this;
+	drawUILabLogo: function() {
+		return new UILabLogo(this);
 	}
 });
